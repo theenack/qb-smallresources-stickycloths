9c0f6ce39ab146d8cd0aca86ad363f42362912fe
theenack <90981544+theenack@users.noreply.github.com>
2023-01-18 08:00:09 -0500

    Create stickyhats.lua

---
+++ stickyhats.lua
@@ -0,0 +1,11 @@
+--Create a new file under qb-smallresources>client
+
+Citizen.CreateThread(function()
+    while true do         
+        if PlayerPedId() ~= lastped then             
+            lastped = PlayerPedId()             
+            SetPedCanLosePropsOnDamage(PlayerPedId(), false, 0)         
+        end         
+        Wait(100)     
+    end 
+end)
